0a1966f2994fd4972d1b658192fa0715e94ecc2a
A L <ultimaterror@laposte.net>
2023-06-12 14:42:08 +0200

    Quest 8 (JWT)

--- app.js
+++ app.js
@@ -12,24 +12,32 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+const { hashPassword, verifyPassword, verifyToken, userIsAllowed } = require("./auth.js");
+
+const { validateUser } = require("./validators.js");
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", validateUser, hashPassword, userHandlers.postUser); // Modifier le validator pour le password
+
 const { validateMovie } = require("./validators.js");
 const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
+
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+
+app.put("/api/users/:id", userIsAllowed, validateUser, hashPassword, userHandlers.updateUser); // Modifier le validator pour le password
+app.delete("/api/users/:id", userIsAllowed, userHandlers.deleteUser);
+
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const { validateUser } = require("./validators.js");
-const userHandlers = require("./userHandlers");
-const { hashPassword } = require("./auth.js");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", validateUser, hashPassword, userHandlers.postUser); // Modifier le validator pour le password
-app.put("/api/users/:id", validateUser, hashPassword, userHandlers.updateUser); // Modifier le validator pour le password
-app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -86,7 +86,7 @@ const updateUser = (req, res) => {
         )
         .then(([result]) => {
             if (result.affectedRows === 0) {
-                res.status(404).send("Not Found");
+                req.user = {email, }
             } else {
                 res.sendStatus(204);
             }
@@ -115,10 +115,31 @@ const deleteUser = (req, res) => {
         });
 };
 
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+    const email = req.body.email
+  
+    database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+        if (users[0] != null) {
+            req.user = users[0];
+
+            next();
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
+
 module.exports = {
     getUsers,
     getUserById,
     postUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    getUserByEmailWithPasswordAndPassToNext
 };
